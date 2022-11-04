9b9966f418beb14236e8f48c5f8c10381228e3af
dogukancem <105238554+dogukancem@users.noreply.github.com>
2022-11-04 18:38:55 +0300

    created axios config files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import PlayerDataService from "./services/player.service";
+const App = () => {
+  var data = {
+    p_id: "test-id-1",
+    p_name: "test-name-1",
+    age: 23,
+    pr: 0,
+    fpr: 0,
+    position_a: "S",
+    position_b: "GK",
+    p_location: "test-location",
+  };
+  PlayerDataService.create(data)
+    .then((response) => {
+      this.setState({
+        p_id: response.data.id,
+        p_name: response.data.name,
+        age: response.data.age,
+        position_a: response.data.positionA,
+        position_b: response.data.positionB,
+        p_location: response.data.location,
+      });
+      console.log(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  return <div className="App"></div>;
+};
 
 export default App;
