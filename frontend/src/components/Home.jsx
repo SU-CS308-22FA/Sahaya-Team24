90d68000296edd79564473c15e8d773ef5dc708d
AhmethanOzcan <ahmethanozcancs@gmail.com>
2022-11-20 23:16:43 +0300

    Email verification

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,6 +7,7 @@ import { Card } from '@mui/material';
 
 import classes from './Home.module.css';
 import Layout from './layout/Layout';
+import { getAuth } from 'firebase/auth';
 
 const Home = () => {
   let navigate = useNavigate();
@@ -15,16 +16,19 @@ const Home = () => {
     navigate('/ProfilePage');
   };
 
-  return (
-    <div>
-    <Layout>
-    <Card className={classes.card}>
-      <h1 className={classes.h1}>Home</h1>
-      <button className={classes.button} onClick={navigateToProfile}>Profile</button>
-      <SignOut/>
-    </Card>
-    </Layout>
-</div>
+  if(getAuth().currentUser.emailVerified) return (
+      <div>
+      <Layout>
+      <Card className={classes.card}>
+        <h1 className={classes.h1}>Home</h1>
+        <button className={classes.button} onClick={navigateToProfile}>Profile</button>
+        <SignOut/>
+      </Card>
+      </Layout>
+  </div>
+    )
+  else return(
+    <div>Please verify your email if already verified reload the page!</div>
   )
 }
 
