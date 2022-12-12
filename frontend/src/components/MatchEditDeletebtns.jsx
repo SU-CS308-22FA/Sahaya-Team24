380b388b0f578ea7732dbf09b0e439cb8daf9727
Egesez <59451387+Egesez@users.noreply.github.com>
2022-12-12 12:34:11 +0300

    delete function added to component

--- frontend/src/components/MatchEditDeletebtns.jsx
+++ frontend/src/components/MatchEditDeletebtns.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Button, AppBar, Toolbar, Typography, List, ListItem, Grid, Stack, Card, Autocomplete, TextField} from '@mui/material';
+import MatchDataService from '../services/match.service'
 
 const MatchEditDeletebtns = (data) => {
 
     let match = data.passedValue._match
+    const handleDeleteMatch = async () => {
+       try{
+        await MatchDataService.delete(match.id);
+        }catch (err) {
+            console.log(err);
+          }
+    }
+
 
     if(data.passedValue.showState){
         console.log(data.passedValue.showState);
      return (
     <div>
         <Button id='editbtn' style={{backgroundColor: "#ffffff", margin:"5px", textTransform:"none"  }}   variant="contained"><Typography style={{color: "#00466e", fontWeight: "bold"}}>Maçı Düzenle</Typography></Button>
-        <Button id= 'dltbtn' style={{backgroundColor: "#ffffff", margin:"5px", textTransform:"none" }} variant="contained"><Typography style={{color: "#00466e", fontWeight: "bold"}}>Maçı Sil</Typography></Button>
+        <Button id= 'dltbtn'  onClick={ () => handleDeleteMatch() } style={{backgroundColor: "#ffffff", margin:"5px", textTransform:"none" }} variant="contained"><Typography style={{color: "#00466e", fontWeight: "bold"}}>Maçı Sil</Typography></Button>
     </div>
      )
     }else{
