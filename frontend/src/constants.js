db65a095a93771614a8a8a6e433cc0829ecee197
AhmethanOzcan <ahmethanozcancs@gmail.com>
2023-01-03 00:57:39 +0300

    Locations On Create Player

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    LOCATION_ARRAY : [
+        'Adalar', 'Arnavutkoy', 'Atasehir', 'Avcilar', 'Bagcilar', 'Bahcelievler', 'Bakirkoy', 'Basaksehir', 'Bayrampasa', 'Besiktas', 'Beykoz', 'Beylikduzu', 'Beyoglu', 'Buyukcekmece', 'Catalca', 'Cekmekoy', 'Esenler', 'Esenyurt', 'Eyupsultan', 'Fatih', 'Gaziosmanpasa', 'Gungoren', 'Kadikoy', 'Kagithane', 'Kartal', 'Kucukcekmece', 'Maltepe', 'Pendik', 'Sancaktepe', 'Sariyer', 'Silivri', 'Sultanbeyli', 'Sultangazi', 'Sile', 'Sisli', 'Tuzla', 'Umraniye', 'Uskudar', 'Zeytinburnu'
+    ]
+})
