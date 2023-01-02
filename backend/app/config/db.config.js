3995852561fa6a4b2270327fb1b947273023627d
dogukancem <105238554+dogukancem@users.noreply.github.com>
2023-01-02 22:15:24 +0300

    Updated database table referees
    - Created new column in referees called available_locations.
    - Updated controller functions to make sure it works with new data type.

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "ademsahaya34", 
-    DB: "sahayadb",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "ankara123",
+  DB: "sahayadb",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
