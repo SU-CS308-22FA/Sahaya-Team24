45642f649bd7105e181798e0cd79e1ab5e5c9859
Egesez <59451387+Egesez@users.noreply.github.com>
2023-01-03 20:41:08 +0300

    end rating
    after pressing "Anasayfaya dön" it terminates the notifiacion and redirects user to homepage with askin are you sure

--- frontend/src/components/RateSendPage.jsx
+++ frontend/src/components/RateSendPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Rating from '@mui/material/Rating';
 import { Button,  Typography, List, ListItem, Grid, Stack, Card, Box} from '@mui/material';
 import MatchDataService from '../services/match.service';
@@ -6,14 +6,18 @@ import PlayerDataService from '../services/player.service';
 import { async } from '@firebase/util';
 import playerService from '../services/player.service';
 import UserRateItem from './UserRateItem';
+import { useNavigate } from 'react-router-dom';
+
 
 
 
 const RateSendPage = (props) => {
-  //const [value, setValue] = React.useState(0);
+  
+  let navigate = useNavigate();
   const msg = props.passedValue.sentVal;
   const [users, setUsers] = React.useState([]);
-
+  const [uID, setUID] = useState(window.localStorage.getItem('user_id'));
+  
   const Retrive_from_database = async (msg) =>{
   //------getting match from database---------------
   // match retrived from the data base is variable match
@@ -62,13 +66,30 @@ React.useEffect( () => {
   
 }, []); 
 
+const navigateToHomepage = ()=>{
+  if(window.confirm("Bu bildirime bir daha ulaşamıyacaksınız çıkmak istediğinize emin misiniz?")){
+    try {
+      playerService.deleteNotification( uID , msg.id);
+    } catch (error) {
+      console.log(error);
+    }
+    navigate('../HomePage');
+
+
+  }
+
+  
+
+}
+
+
   return (
     <Card  style={{ backgroundImage: "url('https://amplex.dk/wp-content/uploads/2016/08/iStock_000022325111Large.jpg')", backgroundSize:"cover", backgroundPosition:"center", height:"100vh", borderRadius:"0"}}>
         <Box sx={{flexGrow: 1}} m = {20} backgroundColor ='#00466e'  >
           <List>
             {users.map((item) => <UserRateItem key ={item.p_id} passedValue = {item}/>)}
           </List>
-        <Button style={{backgroundColor: "#ffffff", margin:"5px", textTransform:"none"  }}   variant="contained"><Typography style={{color: "#00466e", fontWeight: "bold"}}>Yayınla</Typography></Button>
+        <Button onClick={navigateToHomepage}  style={{backgroundColor: "#ffffff", margin:"5px", textTransform:"none"  }}   variant="contained"><Typography style={{color: "#00466e", fontWeight: "bold"}}>Ana Sayfaya Dön</Typography></Button>
         </Box>
     </Card>
   )
