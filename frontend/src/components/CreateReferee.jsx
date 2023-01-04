01270fbd2e799c51bb6d442ce75bb5abddb5de88
dogukancem <105238554+dogukancem@users.noreply.github.com>
2023-01-04 16:25:27 +0300

    Update CreateReferee.jsx

--- frontend/src/components/CreateReferee.jsx
+++ frontend/src/components/CreateReferee.jsx
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import { getAuth } from "firebase/auth";
 import classes from '../components/Mix.module.css';
 import { withRouter } from './withRouter';
-
+import { LOCATION_ARRAY } from '../constants';
 
 class AddReferee extends Component{ 
   constructor(props) {
@@ -116,14 +116,20 @@ class AddReferee extends Component{
       />
       </div>
       <div className={classes.textFieldCss}>
-      <TextField
-        id="input_location"
-        required
-        label="Location"
-        variant="outlined"
-        value={this.state.r_location}
-        onChange={this.onChangeLocation}
-      />
+      <FormControl style={{width:245}}>
+        <InputLabel id="input_location_label">Location</InputLabel>
+        <Select
+          id="input_location"
+          autoWidth
+          value={this.state.r_location}
+          label="Location"
+          onChange={this.onChangeLocation}
+        >
+        {LOCATION_ARRAY.map((location) => (
+          <MenuItem value={location} key = {location}>{location}</MenuItem>
+        ))}
+        </Select>
+      </FormControl>
       </div>
 
       <Button variant="contained" onClick={()=>{
